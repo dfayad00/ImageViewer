17ee3e8e18694e7f4f7e8ccd7af4d3754b3af743
dfayad00 <daniel.fayad@temple.edu>
2022-02-22 21:54:40 -0500

    initial

---
+++ app/src/main/java/edu/temple/imageviewer/ImageActivity.kt
@@ -0,0 +1,13 @@
+package edu.temple.imageviewer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
