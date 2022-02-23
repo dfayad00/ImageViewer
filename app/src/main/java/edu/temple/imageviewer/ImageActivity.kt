6ced42543c03a926ecdca6604d23238b2e7dd32c
dfayad00 <daniel.fayad@temple.edu>
2022-02-22 22:35:22 -0500

    add recyclerview with pictures of dogs

--- app/src/main/java/edu/temple/imageviewer/ImageActivity.kt
+++ app/src/main/java/edu/temple/imageviewer/ImageActivity.kt
@@ -2,12 +2,31 @@ package edu.temple.imageviewer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ImageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val images = arrayOf(
+            R.drawable.pug,
+            R.drawable.bulldog,
+            R.drawable.dalmation,
+            R.drawable.doberman,
+            R.drawable.german_sheperd,
+            R.drawable.golden_retriever,
+            R.drawable.saint_bernard,
+            R.drawable.shih_tzu,
+            R.drawable.rottweiler,
+            R.drawable.yellow_lab)
 
+        val numColumns = 2
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.layoutManager = GridLayoutManager(this, numColumns)
+
+        val adapter = ImageAdapter(images)
+        recyclerView.adapter = adapter
     }
 }
--- app/src/main/java/edu/temple/imageviewer/ImageAdapter.kt
+++ app/src/main/java/edu/temple/imageviewer/ImageAdapter.kt
@@ -1,18 +1,30 @@
 package edu.temple.imageviewer
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 
-class ImageAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+class ImageAdapter(private val data: Array<Int>) :
+    RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var imageView: ImageView = view.findViewById(R.id.image_view)
+
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.image, viewGroup, false)
+
+        return ViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.imageView.setImageResource(data[position])
     }
+
+    override fun getItemCount() = data.size
+
 }
